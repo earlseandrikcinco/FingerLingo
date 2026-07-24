703dd8943689b18ca2cf4e67b617d24b23dbfa8e
Zach-04 <zacharycabalan@gmail.com>
2026-07-24 15:25:56 +0800

    feat: added a camera_test.py

---
+++ tests/camera_test.py
@@ -0,0 +1,26 @@
+from src.models.camera import Camera
+from src.models.hand_detector import HandDetector
+
+
+camera = Camera()
+
+detector = HandDetector()
+
+while True:
+
+    frame = camera.get_frame()
+
+    if frame is None:
+        break
+
+    results = detector.detect(frame)
+
+    detector.draw(frame, results)
+
+    camera.show(frame)
+
+    if camera.should_quit():
+        break
+
+detector.close()
+camera.release()
